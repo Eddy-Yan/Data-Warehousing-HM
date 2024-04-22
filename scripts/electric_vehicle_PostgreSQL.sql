058279a10c7b97eeba22213d86b7bdf6819ffbd3
Eddy-Yan <zunyiyan@gmail.com>
2024-04-21 21:04:20 -0400

    update scheme to PostgreSQL

--- scripts/electric_vehicle_PostgreSQL.sql
+++ scripts/electric_vehicle_PostgreSQL.sql
@@ -1,31 +1,28 @@
 CREATE SCHEMA IF NOT EXISTS "electricvehicle";
 
-CREATE  TABLE "electricvehicle".dim_cafv_eligibility ( 
-	cafv_eligibility_id  integer  NOT NULL  ,
-	CONSTRAINT pk_dim_cafv_eligibility PRIMARY KEY ( cafv_eligibility_id )
- );
-
-CREATE  TABLE "electricvehicle".dim_electric_utility ( 
-	electric_utility_id  integer  NOT NULL  ,
-	CONSTRAINT pk_dim_electric_utility PRIMARY KEY ( electric_utility_id )
+CREATE  TABLE "electricvehicle".dim_cafv_eligibility_type ( 
+	cafv_eligibility_type_id integer  NOT NULL  ,
+	cafv_eligibility_type varchar(255)    ,
+	CONSTRAINT pk_dim_cafv_eligibility PRIMARY KEY ( cafv_eligibility_type_id )
  );
 
 CREATE  TABLE "electricvehicle".dim_electric_vehicle_type ( 
 	electric_vehicle_type_id integer  NOT NULL  ,
+	electric_vehicle_type varchar(255)    ,
 	CONSTRAINT pk_dim_vehicle_type PRIMARY KEY ( electric_vehicle_type_id )
  );
 
 CREATE  TABLE "electricvehicle".dim_location ( 
 	location_id          integer  NOT NULL  ,
-	"state"              varchar(255)    ,
 	county               varchar(255)    ,
 	city                 varchar(255)    ,
-	postal_code          varchar(255)    ,
+	"state"              varchar(255)    ,
+	zip_code             varchar(255)    ,
 	CONSTRAINT pk_dim_location PRIMARY KEY ( location_id )
  );
 
 CREATE  TABLE "electricvehicle".dim_vehicle ( 
-	vin                  varchar(255)  NOT NULL  ,
+	vin                  integer  NOT NULL  ,
 	make                 varchar(255)    ,
 	model                varchar(25)    ,
 	model_year           integer    ,
@@ -38,14 +35,11 @@ CREATE  TABLE "electricvehicle".facts_vehicles (
 	base_msrp            integer    ,
 	location_id          integer  NOT NULL  ,
 	vin                  integer  NOT NULL  ,
-	electric_vehicle_type_id integer    ,
-	cafv_eligibiliby_id  integer    ,
-	electric_utility_id  integer    ,
-	CONSTRAINT pk_facts_vehicles PRIMARY KEY ( fact_id, location_id, vin )
+	electric_vehicle_type_id integer  NOT NULL  ,
+	cafv_eligibiliby_type_id integer    ,
+	CONSTRAINT pk_facts_vehicles PRIMARY KEY ( fact_id )
  );
 
 CREATE UNIQUE INDEX unq_electric_vehicle_type_id ON "electricvehicle".facts_vehicles ( electric_vehicle_type_id );
 
-CREATE UNIQUE INDEX unq_cafv_eligibiliby_id ON "electricvehicle".facts_vehicles ( cafv_eligibiliby_id );
-
-CREATE UNIQUE INDEX unq_electric_utility_id ON "electricvehicle".facts_vehicles ( electric_utility_id );
+CREATE UNIQUE INDEX unq_cafv_eligibiliby_id ON "electricvehicle".facts_vehicles ( cafv_eligibiliby_type_id );
